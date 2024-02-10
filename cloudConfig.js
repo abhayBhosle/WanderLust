1f725747c0a8c4e37e011b24d73aa4505be8ff58
abhayBhosle <abhaybhosle699@gmail.com>
2024-02-10 17:21:44 +0530

    ..

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,7 +1,4 @@
-// const cloudinary = require('cloudinary').v2;
-// const { CloudinaryStorage } = require('multer-storage-cloudinary');
-
-const { v2: cloudinary } = require('cloudinary');
+const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({

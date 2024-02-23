4bc046284f4f62d8e560e7cf3d6d9dc337217916
abhayBhosle <abhaybhosle699@gmail.com>
2024-02-23 19:49:33 +0530

    map

--- public/js/map.js
+++ public/js/map.js
@@ -1,13 +1,20 @@
+
 mapboxgl.accessToken = mapToken;
 
-
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    center: [77.209, 28.6139], // starting position [lng, lat]
-    //center:coordinates,
-    zoom: 9 // starting zoom
+    container: "map", // container ID
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: "mapbox://styles/mapbox/streets-v12", // style URL
+    center: listing1.geometry.coordinates, // starting position [lng, lat]
+    zoom: 6, // starting zoom
+    attributionControl: false,
 });
 
-const marker1 = new mapboxgl.Marker()
-    .setLngLat(coordinates)
+const marker1 = new mapboxgl.Marker({ color: "red" })
+    .setLngLat(listing1.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 }).setHTML(
+            `<h4>${listing1.location}</h4><p>Exact location will be provided after booking</p>`
+        )
+    )
     .addTo(map);
